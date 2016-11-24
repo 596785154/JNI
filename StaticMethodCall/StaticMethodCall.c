2cbd7f4e3bafad88b5eaa9b7fcb3f32dd02cd933
avril <1071920030@qq.com>
2016-11-24 13:55:07 +0800

    static method call

---
+++ StaticMethodCall/StaticMethodCall.c
@@ -0,0 +1,14 @@
+#include <jni.h>
+#include <stdio.h>
+#include "StaticMethodCall.h"
+
+JNIEXPORT void JNICALL Java_StaticMethodCall_nativeMethod
+  (JNIEnv *env, jobject obj){
+	  jclass cls = (*env)->GetObjectClass(env,obj);
+	  jmethodID mid = (*env)->GetStaticMethodID(env,cls,"callback","()V");
+	  if(mid == NULL){
+		  return;
+	  }
+	  printf("In C\n");
+	  (*env)->CallStaticVoidMethod(env,cls,mid);
+  }
